9cb17cfa8c6d0b005d64b6e07e24e8860c02cbbc
ormalky97 <ormalki97@gmail.com>
2020-10-11 19:28:55 +0300

    Fixed UI

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject target;
+    public int type;
+    public float moveSpeed;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GoToTarget();
+    }
+
+    void GoToTarget()
+    {
+        Vector2 dir = target.transform.position - transform.position;
+        rb.velocity = dir.normalized * moveSpeed;
     }
 }
