33684d40eb29b128011bc483582818032803714b
ormalky97 <ormalki97@gmail.com>
2020-10-13 01:57:33 +0300

    UI Updated
    Devided into categories
    Popup tooltip position made dyanmic

---
+++ Assets/CategoryButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CategoryButton : MonoBehaviour
+{
+    public GameObject panel;
+    
+    public void TogglePanel()
+    {
+        GameObject[] panels = GameObject.FindGameObjectsWithTag("Categories");
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(false);
+        }
+
+        panel.SetActive(true);
+    }
+}
