fa236d8d4bec5414d9cf8e3dad00e791808a24d5
ormalky97 <ormalki97@gmail.com>
2020-10-13 21:05:15 +0300

    Distance build system for placer

--- Assets/Scripts/Placer.cs
+++ Assets/Scripts/Placer.cs
@@ -9,8 +9,8 @@ public class Placer : MonoBehaviour
     public float maxDistance;
 
     [Header("Inner Vars")]
-    public GameObject building;
-    public Sites site;
+    public GameObject building = null;
+    public Sites site = null;
 
     //Refs
     List<GameObject> stations;
@@ -26,50 +26,68 @@ public class Placer : MonoBehaviour
     {
         tooltip = GameObject.FindObjectOfType<Messages>();
         stations = GameObject.FindObjectOfType<BuildingsManager>().stations;
+        spr = GetComponent<SpriteRenderer>();
+        cam = Camera.main;
+        res = GameObject.Find("Game Manager").GetComponent<Resources>();
+        
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        spr = GetComponent<SpriteRenderer>();
+        site = building.GetComponent<Sites>();
         spr.sprite = building.GetComponent<SpriteRenderer>().sprite;
 
-        cam = Camera.main;
-        res = GameObject.Find("Game Manager").GetComponent<Resources>();
-        site = building.GetComponent<Sites>();
+        foreach (GameObject station in stations)
+        {
+            station.GetComponent<Station>().ShowRange();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector2(Mathf.Round(cam.ScreenToWorldPoint(Input.mousePosition).x), Mathf.Round(cam.ScreenToWorldPoint(Input.mousePosition).y));
-
-        if (CheckPlace())
+        if(CheckDistance())
         {
-            spr.color = new Color(1, 1, 1, 0.75f);
-            if (Input.GetMouseButtonDown(0))
+            if (CheckPlace())
             {
-                Build();
+                spr.color = new Color(1, 1, 1, 0.75f);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Build();
+                }
+            }
+            else
+            {
+                spr.color = new Color(1, 0, 0, 0.75f);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    tooltip.ShowMessage(reason, new Color(1, 1, 1, 1));
+                }
+            }
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                Destroy(gameObject);
             }
         }
         else
         {
             spr.color = new Color(1, 0, 0, 0.75f);
-            if (Input.GetMouseButtonDown(0))
-            {
-                tooltip.ShowMessage(reason, new Color(1, 1, 1, 1));
-            }
+            if(Input.GetMouseButtonDown(0))
+                tooltip.ShowMessage("Can't build so far from a station", new Color(1, 1, 1, 1));
         }
-
-        if(Input.GetMouseButtonDown(1))
-        {
-            Destroy(gameObject);
-        }
+        
     }
 
     bool CheckDistance()
     {
-        return false;
+        GameObject closetStation = FindClosetStation();
+        if (Vector2.Distance(transform.position, closetStation.transform.position) <= maxDistance)
+            return true;
+        else
+            return false;
     }
 
     GameObject FindClosetStation()
@@ -110,4 +128,11 @@ public class Placer : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        foreach (GameObject station in stations)
+        {
+            station.GetComponent<Station>().HideRange();
+        }
+    }
 }
---
+++ Assets/Station.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station : MonoBehaviour
+{
+    GameObject rangeRenderer;
+    float range = 20f;
+
+    private void Awake()
+    {
+        rangeRenderer = transform.GetChild(0).gameObject;
+        rangeRenderer.transform.localScale = new Vector2(range, range);
+    }
+
+    public void ShowRange()
+    {
+        rangeRenderer.SetActive(true);
+    }
+
+    public void HideRange()
+    {
+        rangeRenderer.SetActive(false);
+    }
+}
