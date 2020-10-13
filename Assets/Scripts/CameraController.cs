30185f6cf49c64fbe23550d03e72c73e03e56ee9
ormalky97 <ormalki97@gmail.com>
2020-10-13 03:33:50 +0300

    Background
    added Background Texture
    implemented scrolling BG

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
@@ -9,10 +10,15 @@ public class CameraController : MonoBehaviour
     public float maxZoom;
     public float minZoom;
 
+    [Header("Background")]
+    public GameObject background;
+    public RawImage rawImage;
+
     //Refs & Vars
     Camera cam;
     Vector3 movement;
     float zoom;
+    
 
     private void Awake()
     {
@@ -25,12 +31,14 @@ public class CameraController : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         zoom = Input.mouseScrollDelta.y;
-        
+
+        background.transform.position = new Vector2(transform.position.x, transform.position.y);
     }
 
     private void FixedUpdate()
     {
         transform.position = transform.position + movement * moveSpeed;
+        rawImage.uvRect = new Rect(rawImage.uvRect.x + movement.x * 0.05f, rawImage.uvRect.y + movement.y * 0.088f, rawImage.uvRect.width, rawImage.uvRect.height);
 
         cam.orthographicSize += zoom * -1;
         if (cam.orthographicSize > maxZoom)
