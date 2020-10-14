85741fb5daecdb972e6806dbf0e1bd078c5355ce
ormalky97 <ormalki97@gmail.com>
2020-10-14 15:14:05 +0300

    Main menu WIP

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Background")]
+    public RawImage background;
+    public float xFactor;
+    public float yFactor;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newX = background.uvRect.x + xFactor * Time.deltaTime;
+        float newY = background.uvRect.y + yFactor * Time.deltaTime;
+        background.uvRect = new Rect(newX, newY, background.uvRect.width, background.uvRect.height);
+    }
+}
